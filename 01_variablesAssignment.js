209ed0d1a0dc35de41fb091c0b54450361eb54b4
vijayphad <vijayphad18@gmail.com>
2023-08-21 16:52:42 +0530

    variables in js
    variables in js

---
+++ 01_variablesAssignment.js
@@ -0,0 +1,33 @@
+var myFullName;
+myFullName = "VIJAY PHAD";
+console.log(myFullName);
+myFullName = "VIJAY DINKAR PHAD";
+console.log(myFullName);
+
+
+
+
+var myLove = "I LOVE ONLY JS";
+console.log(myLove);
+
+
+var firstName = "VIJAY";
+console.log(firstName);
+
+var lastName = "PHAD";
+console.log(lastName);
+
+var age = 23;
+console.log(age);
+
+var address = "pune";
+console.log(address);
+
+var pinCode = 413512;
+console.log(pinCode);
+
+var gender = "male";
+console.log(gender);
+
+var isMarried = false;
+console.log(isMarried);
