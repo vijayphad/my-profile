b7501020088933d729bebc5663e0b9f5ba67da9e
vijayphad <vijayphad18@gmail.com>
2023-08-22 11:57:15 +0530

    practice of typeOf operator
    practice of typeOf operator

---
+++ 02_typeOf.js
@@ -0,0 +1,29 @@
+
+var bankName;
+console.log(bankName);
+bankName = "SBI";
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks = 90;
+console.log(marks);
+
+
+var isWorling;
+console.log(isWorling);//undefined because it is  not initialized with value it is only declared
+console.log(typeof isWorling); 
+
+isWorling = false;
+console.log(isWorling);
+console.log(typeof isWorling);
+
+
+var totalCount = "one hundred and seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
+
+
